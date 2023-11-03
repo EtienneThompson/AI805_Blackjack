bf45959a31e45656c60982087451f2674af8909c
Kevin Kang <whenlove01@naver.com>
2023-11-03 19:55:25 -0400

    Q-Learning part updated - In progress

--- KevinAgent.py
+++ KevinAgent.py
@@ -6,6 +6,38 @@ import card_methods
 
 class KevinAgent(BaseAgent):
     DEFAULT_BET = 50  # This will be the default betting amount
+    EPSILON_DECAY = 0.99  # Decay factor for exploration rate. To reduce agent's exploration rate
+    MIN_EPSILON = 0.1  # Minimum exploration rate. Minimum value that epsilon can decrease to. 
+    
+    ######### Initialize Q-Table ############# 
+    def __init__(self):
+        super().__init__()
+        # existing initialization code
+        # missing key will have default value of 0.0. This is to avoid KeyError when accessing a qdict with a key that don't exist
+        self.q_table = qdick(lambda: qdict(float))
+        self.alpha = 0.1 # learning rate
+        self.gamma = 0.9 # discount factor 
+    
+    ######### Set Policy #####################  
+    def epsilon_greedy_policy(self, state, epsilon=0.1):
+        if random.random() < epsilon: #The vaule of epsilon is between 0 and 1. 
+            return random.choice([Enums.AgentStates.HIT, Enums.AgentStates.STAND, Enums.AgentStates.DOUBLE_DOWN, Enums.AgentStates.SPLIT])
+        else: #in this case the agent will try to exploit what it has learned so far
+            # Return the action with the highest Q-Value for the current state
+            return max(self.q_table[state], key=self.q_table[state].get)
+    
+    ######### Learning algorithm #############
+    def learn(self, state, action, reward, next_state):
+        old_value = self.q_table[state][action] # takes current Q-value from the Q-table for the given 'state' and 'action' 
+        next_max = max(self.q_table[next_state].values()) # maximum Q-value for the next state(best possible future reward)
+        
+        # Q-Learning formula
+        # new Q-value is the weighted sum of the old value and the learned value. 
+        # The learned value is immediate reward plus the discounted maximum future reward. 
+        #This part calculates the learning rate. 
+        new_value = (1 - self.alpha) * old_value + self.alpha * (reward + self.gamma * next_max)
+        # Discount factor which determines importance of future rewards. 
+        self.q_table[state][action] = new_value
 
     def place_bet(self, hand):  # this is new betting function
         """Decide how much to bet based on the current hand."""
