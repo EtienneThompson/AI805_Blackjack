3438d9a8350059ff7718636774223788d12cf8d3
Etienne Thompson <et@etiennethompson.com>
2023-11-15 18:56:10 -0800

    Fix double down logic.

--- EtienneAgentTree/GameTree.py
+++ EtienneAgentTree/GameTree.py
@@ -1,4 +1,5 @@
 """Represents the game tree for a blackjack move."""
+import random
 from EtienneAgentTree import DecisionNode, RandomNode, BaseNode
 import card_methods
 
@@ -12,31 +13,46 @@ class GameTree:
         self.max_depth = 4
         self._cards = cards
         self.root = BaseNode.BaseNode(self._cards)
-        self.generate_game_tree(self.root, 0)
+        self.generate_game_tree(self.root, 0, False)
 
         self.traversed_nodes = 0
 
-    def generate_game_tree(self, node, depth):
+    def generate_game_tree(self, node, depth, stop_generating):
         """Recursively generate the game tree from the current state."""
         if depth >= self.max_depth:
             return
 
-        possible_decisions = ["STAND", "HIT", "DOUBLE_DOWN"]
+        possible_decisions = ["STAND", "HIT"]
+
+        if len(node.get_cards()) == 2:
+            possible_decisions.append("DOUBLE_DOWN")
+
         possible_ranks = ["2", "3", "4", "5", "6",
                           "7", "8", "9", "10", "J", "Q", "K", "A"]
-
         if depth % 2 == 0:
             # self._debug("Adding decision nodes...")
             # Decision tree node
             if node.get_hand_value() < 21:
                 if card_methods.can_split_hand(node.get_cards()):
                     possible_decisions.append("SPLIT")
-                for decision in possible_decisions:
-                    decision = DecisionNode.DecisionNode(
-                        node.get_cards(), decision)
-                    node.add_child(decision)
 
-                    self.generate_game_tree(decision, depth + 1)
+                # Shuffle the order of the decisions to randomize decisions with
+                # matching probabilities.
+                for i in range(10):
+                    swap_index = random.randint(0, len(possible_decisions) - 1)
+                    temp = possible_decisions[0]
+                    possible_decisions[0] = possible_decisions[swap_index]
+                    possible_decisions[swap_index] = temp
+
+                for decision in possible_decisions:
+                    decision_node = DecisionNode.DecisionNode(
+                        node.get_cards(), decision)
+                    node.add_child(decision_node)
+
+                    if decision == "STAND" or decision == "DOUBLE_DOWN":
+                        self.generate_game_tree(decision_node, depth + 1, True)
+                    else:
+                        self.generate_game_tree(decision_node, depth + 1, False)
 
         if depth % 2 == 1:
             # Chance tree node
@@ -48,18 +64,19 @@ class GameTree:
                 for i in range(2):
                     self._debug(f"Handling hand {i} for a split node")
                     for rank in possible_ranks:
-                        random = RandomNode.RandomNode(
+                        random_node = RandomNode.RandomNode(
                             [node.get_cards()[i]], rank + "♥", 1 / 26, node.is_final_decision())
-                        node.add_child(random)
+                        node.add_child(random_node)
             elif node.get_hand_value() < 21:
                 for rank in possible_ranks:
                     # self._debug(
                     #     f"node type: {node.type} - is final decision: {node.is_final_decision()}")
-                    random = RandomNode.RandomNode(
-                        node.get_cards(), rank + "♥", 1 / 13, node.is_final_decision())
-                    node.add_child(random)
+                    random_node = RandomNode.RandomNode(
+                        node.get_cards(), rank + "♥", 1 / 13, node.type == "STAND")
+                    node.add_child(random_node)
 
-                    self.generate_game_tree(random, depth + 1)
+                    if not stop_generating:
+                        self.generate_game_tree(random_node, depth + 1, False)
 
     def get_leaf_nodes(self, node):
         """Return all the leaf nodes of the tree."""
@@ -103,8 +120,8 @@ class GameTree:
             elif isinstance(node, RandomNode.RandomNode):
                 # Since it's a random node, the decision will be made in the parent node.
                 # self._debug(
-                #     f"Random node leaf at depth {depth}: \"\", is_final_chance node: {node.is_final_chance()}, {node.get_node_weight(reverse_weight=node.is_final_chance())}, card: {node.get_cards()}")
-                return ["", node.get_node_weight(reverse_weight=node.is_final_chance())]
+                #     f"Random node leaf at depth {depth}: is_final_chance node: {node.is_stand_decision()}, {node.get_node_weight(reverse_weight=node.is_stand_decision())}, card: {node.get_cards()}")
+                return ["", node.get_node_weight(reverse_weight=node.is_stand_decision())]
             else:
                 # This happens when we have no decision tree because we have a 21
                 # in our hand already.
@@ -112,21 +129,22 @@ class GameTree:
 
         if isinstance(node, DecisionNode.DecisionNode):
             weight = 0
-            # self._debug(
-            #     f"Checking weight for decision node at depth {depth}: {node.type}")
+            self._debug(
+                f"Checking weight for decision node at depth {depth}: {node.type}")
             for child in node.get_children():
                 _, value = self._expectminimax(child, depth+1)
                 weight += child.probability * value
-            # self._debug(
-            #     f"Decision Node at depth {depth}: {node.type}, {weight}")
+            self._debug(
+                f"Decision Node at depth {depth}: {node.type}, {weight}")
             return [node.type, weight]
         elif isinstance(node, RandomNode.RandomNode) or isinstance(node, BaseNode.BaseNode):
             options = list()
+            self._debug(f"Checking weight for random node at depth {depth}")
             for child in node.get_children():
                 options.append(self._expectminimax(child, depth+1))
             max_node = max(options, key=lambda x: x[1])
-            # self._debug(
-            #     f"Random Node at depth {depth}: {max_node[0]}, {max_node[1]}")
+            self._debug(
+                f"Random Node at depth {depth}: {max_node[0]}, {max_node[1]}")
             return max_node
 
         # This case shouldn't happen, but just have a fallback in case.
